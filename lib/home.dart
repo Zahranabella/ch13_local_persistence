5c811f7e15b4be8f3b82cb75071e37a540c49025
Zahranabella <zhranabella@gmail.com>
2023-11-14 10:03:58 +0700

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Home());
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Local Persistence',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        bottomAppBarTheme: const BottomAppBarTheme(color: Colors.blue),
+      ),
+      home: const Home(),
+    );
+  }
+}
